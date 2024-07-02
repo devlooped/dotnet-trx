ae64ef27937f0abb9b2c134267d1b53350fd63dc
Daniel Cazzulino <daniel@cazzulino.com>
2024-07-02 00:11:24 -0300

    Initial basic renderer for recursive *.trx files in a directory

---
+++ src/dotnet-trx/Program.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using NuGet.Protocol.Core.Types;
+using NuGet.Configuration;
+using System;
+using NuGet.Versioning;
+using Spectre.Console.Cli;
+using Devlooped;
+using Spectre.Console;
+
+var app = new CommandApp<TrxCommand>();
+
+// Alias -? to -h for help
+if (args.Contains("-?"))
+    args = args.Select(x => x == "-?" ? "-h" : x).ToArray();
+
+app.Configure(config => config.SetApplicationName(ThisAssembly.Project.ToolCommandName));
+
+if (args.Contains("--version"))
+{
+    AnsiConsole.MarkupLine($"{ThisAssembly.Project.ToolCommandName} version [lime]{ThisAssembly.Project.Version}[/] ({ThisAssembly.Project.BuildDate})");
+    AnsiConsole.MarkupLine($"[link]{ThisAssembly.Git.Url}/releases/tag/{ThisAssembly.Project.BuildRef}[/]");
+
+    foreach (var message in await CheckUpdates(args))
+        AnsiConsole.MarkupLine(message);
+
+    return 0;
+}
+
+var updates = Task.Run(() => CheckUpdates(args));
+var exit = app.Run(args);
+
+if (await updates is { Length: > 0 } messages)
+{
+    foreach (var message in messages)
+        AnsiConsole.MarkupLine(message);
+}
+
+return exit;
+
+static async Task<string[]> CheckUpdates(string[] args)
+{
+    if (args.Contains("-u") && !args.Contains("--unattended"))
+        return [];
+
+    var providers = Repository.Provider.GetCoreV3();
+    var repository = new SourceRepository(new PackageSource("https://api.nuget.org/v3/index.json"), providers);
+    var resource = await repository.GetResourceAsync<PackageMetadataResource>();
+    var localVersion = new NuGetVersion(ThisAssembly.Project.Version);
+    var metadata = await resource.GetMetadataAsync(ThisAssembly.Project.PackageId, true, false,
+        new SourceCacheContext
+        {
+            NoCache = true,
+            RefreshMemoryCache = true,
+        },
+        NuGet.Common.NullLogger.Instance, CancellationToken.None);
+
+    var update = metadata
+        .Select(x => x.Identity)
+        .Where(x => x.Version > localVersion)
+        .OrderByDescending(x => x.Version)
+        .Select(x => x.Version)
+        .FirstOrDefault();
+
+    if (update != null)
+    {
+        return [
+            $"There is a new version of [yellow]{ThisAssembly.Project.PackageId}[/]: [dim]v{localVersion.ToNormalizedString()}[/] -> [lime]v{update.ToNormalizedString()}[/]",
+            $"Update with: [yellow]dotnet[/] tool update -g {ThisAssembly.Project.PackageId}"
+        ];
+    }
+
+    return [];
+}
---
+++ src/dotnet-trx/TrxCommand.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using Devlooped.Web;
+using Humanizer;
+using Spectre.Console.Cli;
+using static Spectre.Console.AnsiConsole;
+
+namespace Devlooped;
+
+public class TrxCommand : Command<TrxCommand.TrxSettings>
+{
+    public class TrxSettings : CommandSettings
+    {
+        [Description("Optional base directory for *.trx files discovery. Defaults to current directory.")]
+        [CommandArgument(0, "[PATH]")]
+        public string? Path { get; init; }
+
+        [Description("Recursively search for *.trx files.")]
+        [CommandOption("-r|--recursive")]
+        [DefaultValue(true)]
+        public bool Recursive { get; init; } = true;
+    }
+
+    public override int Execute(CommandContext context, TrxSettings settings)
+    {
+        var path = settings.Path ?? Directory.GetCurrentDirectory();
+        var search = settings.Recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+
+        foreach (var trx in Directory.EnumerateFiles(path, "*.trx", search))
+        {
+            using var file = File.OpenRead(trx);
+            // Clears namespaces
+            var doc = HtmlDocument.Load(file, new HtmlReaderSettings { CaseFolding = Sgml.CaseFolding.None });
+            foreach (var result in doc.CssSelectElements("UnitTestResult").OrderBy(x => x.Attribute("testName")?.Value))
+            {
+                var test = result.Attribute("testName")?.Value;
+                switch (result.Attribute("outcome")?.Value)
+                {
+                    case "Passed":
+                        MarkupLine($":check_mark_button: {test}");
+                        break;
+                    case "Failed":
+                        MarkupLine($":cross_mark_button: {test}");
+                        break;
+                    case "Skipped":
+                        MarkupLine($":fast_forward_button: {test}");
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            var counters = doc.CssSelectElement("ResultSummary > Counters");
+            var times = doc.CssSelectElement("Times");
+            if (counters == null || times == null)
+                continue;
+
+            var total = int.Parse(counters.Attribute("total")?.Value ?? "0");
+            var passed = int.Parse(counters.Attribute("passed")?.Value ?? "0");
+            var failed = int.Parse(counters.Attribute("failed")?.Value ?? "0");
+            var skipped = int.Parse(counters.Attribute("skipped")?.Value ?? "0");
+
+            var start = DateTime.Parse(times.Attribute("start")?.Value!);
+            var finish = DateTime.Parse(times.Attribute("finish")?.Value!);
+            var duration = finish - start;
+
+            WriteLine();
+
+            Markup($":backhand_index_pointing_right: Run {total} tests in ~ {duration.Humanize()}");
+
+            if (failed > 0)
+                MarkupLine($" :cross_mark_button:");
+            else
+                MarkupLine($" :check_mark_button:");
+
+            if (passed > 0)
+                MarkupLine($"  :check_mark_button: {passed} passed");
+
+            if (failed > 0)
+                MarkupLine($"  :cross_mark_button: {failed} failed");
+
+            if (skipped > 0)
+                MarkupLine($"  :fast_forward_button: {skipped} skipped");
+        }
+
+        return 0;
+    }
+}
